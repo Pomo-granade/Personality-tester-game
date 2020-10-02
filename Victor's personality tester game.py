e8c1fd09836a3b86f944ff2de70afdcc2c6876f1
Pomo-granade <72248168+Pomo-granade@users.noreply.github.com>
2020-10-02 13:28:11 +0530

    Update Victor's personality tester game.py

--- Victor's personality tester game.py
+++ Victor's personality tester game.py
@@ -2,14 +2,7 @@ print("\nWELCOME TO PERSONALITY TESTER GAME\n")
 print("Note - CHOOSE THE MATCHING ANSWER AND WRITE THE NUMBER")
 print("\nQ.You enjoy social event with lots of people?")
 print("""1.Not at all
-2.Sometimes
-3.Yes most of the time
-4.Hell yeah always""")
-ans1 = int(input("->"))
-
-print("\nQ.You don't spent time exploring intriguing ideas.")
-print("""1.Not at all
-2.Sometimes
+2.Som
 3.Yes most of the time
 4.Hell yeah always""")
 ans2 = int(input("->"))
@@ -17,48 +10,24 @@ ans2 = int(input("->"))
 
 print("\nQ.You start the conversation.")
 print("""1.Not at all
-2.Sometimes
-3.Yes most of the time
-4.Hell yeah always""")
-ans3 = int(input("->"))
-
-
+2.Someti
 
 print("\nQ.You like outings with friends.")
 print("""1.Not at all
 2.Sometimes
 3.Yes most of the time
-4.Hell yeah always""")
-ans4 = int(input("->"))
-
-print("\nQ.You are imprecise.") 
-print("""1.Not at all
+4.Hell yeah alys""")
+ans4 = intu are imprecise.") 
+primost of the time
+4.H
 2.Sometimes
 3.Yes most of the time
-4.Hell yeah always""")
-ans5 = int(input("->"))
-
-
-print("\nQ.You feel bored when alone.")
-print("""1.Not at all
-2.Sometimes
-3.Yes most of the time
-4.Hell yeah always""")
-ans6 = int(input("->"))
-
-
+4.Hell yeah always
 print("\nQ.You have a careful and methodical approach to life.")
-print("""1.Not at all
-2.Sometimes
-3.Yes most of the time
-4.Hell yeah always""")
+print(always""")
 ans7 = int(input("->"))
 
 if 1<(ans1 + ans2 + ans3 + ans4 + ans5 + ans6 + ans7)<14:
     print("\nYou are an introvert")
 
-elif 13<(ans1 + ans2 + ans3 + ans4 + ans5 + ans6 + ans7)<20:
-    print("\nYou are an Ambivert")
-
-else:
-    print("\nYou are an Extrovert")
+elif 13<(ans1 + ans2 + ans3 
